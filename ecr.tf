af5803ca78f2fff86bf008d6e7710ba4af3d99a7
Peter Obijiofor <108196402+DoingsLLC@users.noreply.github.com>
2023-12-19 12:47:57 -0500

    Delete ecr.tf

--- ecr.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "this" {
-  name                 = var.application_name
-  image_tag_mutability = "IMMUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
